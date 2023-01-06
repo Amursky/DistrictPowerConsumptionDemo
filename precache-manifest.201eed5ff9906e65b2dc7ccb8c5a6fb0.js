62da4a0801326faf08d829be6da942cc34a3d048
Amursky <121831891+Amursky@users.noreply.github.com>
2023-01-06 11:02:30 +0200

    Add files via upload

---
+++ precache-manifest.201eed5ff9906e65b2dc7ccb8c5a6fb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "06c5ddc9564a51df1be2",
+    "url": "/DistrictPowerConsumptionDemo/static/css/main.a618d28a.chunk.css"
+  },
+  {
+    "revision": "06c5ddc9564a51df1be2",
+    "url": "/DistrictPowerConsumptionDemo/static/js/main.b4a4eb99.chunk.js"
+  },
+  {
+    "revision": "f9100d43d50a28ef6b0c",
+    "url": "/DistrictPowerConsumptionDemo/static/js/runtime~main.6f2dbe7e.js"
+  },
+  {
+    "revision": "81b054a7a5b2604d80b2",
+    "url": "/DistrictPowerConsumptionDemo/static/css/2.0e3e38ac.chunk.css"
+  },
+  {
+    "revision": "81b054a7a5b2604d80b2",
+    "url": "/DistrictPowerConsumptionDemo/static/js/2.4c025b1c.chunk.js"
+  },
+  {
+    "revision": "2a84fab6fb49529ba53a8541b8ed39e5",
+    "url": "/DistrictPowerConsumptionDemo/index.html"
+  }
+];
